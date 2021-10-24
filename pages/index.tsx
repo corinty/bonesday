465b261c1dbcf1deef2f91e5b5faa1209df1eded
Corin McHargue <corin.mchargue@gmail.com>
2021-10-24 14:16:42 -0500

    chagnes test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,25 @@
+/* eslint-disable react/no-unescaped-entities */
 import type { NextPage } from "next";
+import Image from "next/image";
 import { useState } from "react";
-import styles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => {
     const [isBonesDay, setIsBonesDay] = useState(true);
     return (
-        <main className={styles.main}>
-            <button onClick={() => setIsBonesDay(!isBonesDay)}>
-                It's a {isBonesDay ? "no" : ""}bones day!
-            </button>
-        </main>
+        <>
+            <main>
+                <div className="reading">
+                    <button onClick={() => setIsBonesDay(!isBonesDay)}>
+                        It's a {isBonesDay ? "no" : ""}bones day!
+                    </button>
+                </div>
+
+                <a href="https://www.tiktok.com/@jongraz?" className="social-share">
+                    <img src="/tiktok_logo.png" layout="fill" />
+                    @jongraz
+                </a>
+            </main>
+        </>
     );
 };
 
